652b23413bdc6acc6af38b9ff13d43f0077db3d1
KekemonBS <sasha007.h@gmail.com>
2020-03-24 14:16:46 +0200

    v(3)

--- LAB_3.py
+++ LAB_3.py
@@ -131,11 +131,6 @@ t1 = abs(beta1) / Sb
 t2 = abs(beta2) / Sb
 t3 = abs(beta3) / Sb
 T_list = [t0, t1, t2, t3]
-student_table = [12.71, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365,
-				 2.306, 2.262, 2.228, 2.201, 2.179, 2.160, 2.145,
-				 2.131, 2.12,  2.11,  2.101, 2.093, 2.086, 2.08,
-				 2.074, 2.069, 2.064, 2.06, 2.056, 2.052,  2.048,
-				 2.045, 2.042];
 T = t.ppf((1 + (1 - q)) / 2, f3)
 print("T = "+str(T)+ "\nT_list = "+str(list(map(lambda x : round(x,2),T_list))))
 for i in range(len(T_list)):
